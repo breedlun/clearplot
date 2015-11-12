5f98c39eab8ca24e1e71df5ec16ec62c3e54b496
breedlun <breedlun@gmail.com>
2015-11-12 04:23:46 -0700

    Update setup.py with the proper home page.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'Clearplot creates publication quality plots using matplotlib',
   author = 'Benjamin Reedlunn',
   author_email = 'breedlun@gmail.com',
-  url = 'https://github.com/breedlun/clearplot', # use the URL to the github repo
-  download_url = 'https://github.com/breedlun/clearplot/tarball/1.0', # I'll explain this in a second
-  keywords = ['matplotlib', 'plotting'], # arbitrary keywords
+  url = 'http://clearplot.readthedocs.org',
+  download_url = 'https://github.com/breedlun/clearplot/tarball/1.0',
+  keywords = ['matplotlib', 'plotting'],
   classifiers = [],
 )
