6d13b7bdd33466f449bdfcb708e11d8970bc1ff0
breedlun <breedlun@gmail.com>
2015-11-30 07:32:41 -0700

    Added a release checklist, updated the version in , and made the version a variable in .

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
+
+#Define the version of clearplot
+cp_version = '1.0.2'
+
 setup(
   name = 'clearplot',
   packages = ['clearplot'], # this must be the same as the name above
-  version = '1.0.2',
+  version = cp_version,
   description = 'Clearplot creates publication quality plots using matplotlib',
   author = 'Benjamin Reedlunn',
   author_email = 'breedlun@gmail.com',
   url = 'http://clearplot.readthedocs.org',
-  download_url = 'https://github.com/breedlun/clearplot/tarball/1.0.2' ,
+  download_url = 'https://github.com/breedlun/clearplot/tarball/' + cp_version,
   keywords = ['matplotlib', 'plotting'],
   classifiers = [],
 )
