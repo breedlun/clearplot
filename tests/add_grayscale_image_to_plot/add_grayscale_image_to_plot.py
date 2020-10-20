f6da387530d8b9ed8e75597e91c145e7cb5f001b
Benjamin <breedlu@sandia.gov>
2020-10-20 17:23:03 -0600

    Changed add_grayscale_image_to_plot test to use matplotlib's imread function because scipy's imread is deprecated.  Also eliminated the comment about https://github.com/matplotlib/matplotlib/issues/4260 since it appears to be resolved.

--- tests/add_grayscale_image_to_plot/add_grayscale_image_to_plot.py
+++ tests/add_grayscale_image_to_plot/add_grayscale_image_to_plot.py
@@ -7,14 +7,15 @@ Created on Mon May 30 21:04:47 2016
 
 import clearplot.figure as cpfig
 import clearplot as cp
-import os, scipy.misc
+import os
+import matplotlib.pyplot
 data_dir = os.path.join(os.path.dirname(os.path.dirname(cp.__file__)), \
     'doc', 'source', 'data')
 
 #Verification of adding an image and adding a color bar
 #Load image data
 path = os.path.join(data_dir, 'fiber_image.tiff')
-im = scipy.misc.imread(path)
+im = matplotlib.pyplot.imread(path)
 fig = cpfig.Figure()
 ax = fig.add_axes()
 ax.x_label = ['x', 'pix']
@@ -23,8 +24,3 @@ im_obj = ax.add_image(im, im_interp = 'bilinear', c_map = cp.colors.c_maps['ligh
 ax.x_tick = 50
 fig.add_color_bar(im_obj)
 fig.auto_adjust_layout()
-#As reported in issue #4260 on Github 
-#(https://github.com/matplotlib/matplotlib/issues/4260) matplotlib makes the
-#axes background black when saving pdfs, with 1 image, grayscale colormaps, and
-#transparent background.  So we avoid this by making the background opaque.
-fig.save('add_grayscale_image_to_plot', transparent = False)
